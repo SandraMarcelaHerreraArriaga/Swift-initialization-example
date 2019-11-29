83dc82cb3863fdb6bcae6006dbbdcbdbb21f45b3
sandra <sandra.h.arriaga@gmail.com>
2019-11-28 23:57:42 -0600

    added custom initializer

--- BastOff.playground/Contents.swift
+++ BastOff.playground/Contents.swift
@@ -30,5 +30,21 @@ extension RocketStageConfiguration{
 let stageOneConfiguration = RocketStageConfiguration(propellantMass: 119.1,
 liquidOxygenMass: 276.0, nominalBurnTime: 180)
 
+struct Weather {
+  let temperatureCelsius: Double
+  let windSpeedKilometersPerHour: Double
+    
+    init(temperatureFahrenheit: Double = 72, windSpeedMilesPerHour: Double = 5) {
+      self.temperatureCelsius = (temperatureFahrenheit - 32) / 1.8
+      self.windSpeedKilometersPerHour = windSpeedMilesPerHour * 1.609344
+    }
+
+}
+let currentWeather = Weather(temperatureFahrenheit: 87, windSpeedMilesPerHour: 2)
+
+currentWeather.temperatureCelsius
+currentWeather.windSpeedKilometersPerHour
+
+
 
 
