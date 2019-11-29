3323ab670828a22d1f9fac7e414e88c7d7001bd0
sandra <sandra.h.arriaga@gmail.com>
2019-11-28 23:18:12 -0600

    init RocketConfiguration struct

---
+++ BastOff.playground/Contents.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct RocketConfiguration {
+    
+    let name: String = "Athena 9 Heavy"
+    let numberOfFirstStageCores: Int = 3
+    let numberOfSecondStageCores = 1
+    let numberOfStageReuseLandIngLegs: Int? = nil
+    
+}
+let athena9Heavy = RocketConfiguration()
